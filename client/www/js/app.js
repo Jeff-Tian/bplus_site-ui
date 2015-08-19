13467ea52360dcf9da250000d2dc391da97cedb1
faceach <faceach@gmail.com>
2015-08-20 00:11:42 +0800

    homepage ui

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -2,27 +2,26 @@
 define([], function() {
   return {
     start: function() {
-          //$(document)
-    //  .ready(function() {
-            // fix menu when passed
-            $('.masthead')
-                .visibility({
-                    once: false,
-                    onBottomPassed: function() {
-                        $('.fixed.menu').transition('fade in');
-                    },
-                    onBottomPassedReverse: function() {
-                        $('.fixed.menu').transition('fade out');
-                    }
-                });
-            // create sidebar and attach to menu open
-            $('.ui.sidebar')
-                .sidebar('attach events', '.toc.item');
-    //  });
+      //$(document)
+      //  .ready(function() {
+      // fix menu when passed
+      $('.masthead')
+        .visibility({
+          once: false,
+          onBottomPassed: function() {
+            $('.fixed.menu').transition('fade in');
+          },
+          onBottomPassedReverse: function() {
+            $('.fixed.menu').transition('fade out');
+          }
+        });
+      // create sidebar and attach to menu open
+      $('.ui.sidebar')
+        .sidebar('attach events', '.toc.item');
+      //  });
       // TODO: initial tabs, to be integrated into JS framework
       $('.menu .item')
-        .tab()
-      ;
+        .tab();
     }
   }
 });
