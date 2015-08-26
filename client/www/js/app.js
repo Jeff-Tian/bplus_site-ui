f523b12d7d98dd1b976ccc11814706810490a34f
田杰 <jefftian@hcdlearning.com>
2015-08-26 14:57:14 +0800

    Brid-95: Add register page.

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -1,29 +1,29 @@
 // TODO: initial scripts, to be integrated into JS framework
-define([], function() {
-  return {
-    start: function() {
-      //$(document)
-      //  .ready(function() {
-      // fix header when passed
-      $('.b-masthead')
-        .visibility({
-          once: false,
-          onBottomPassed: function() {
-            $('[data-action=fixedHeader]').transition('fade in');
-          },
-          onBottomPassedReverse: function() {
-            $('[data-action=fixedHeader]').transition('fade out');
-          }
-        });
+define([], function () {
+    return {
+        start: function () {
+            //$(document)
+            //  .ready(function() {
+            // fix header when passed
+            $('.b-masthead')
+                .visibility({
+                    once: false,
+                    onBottomPassed: function () {
+                        $('[data-action=fixedHeader]').transition('fade in');
+                    },
+                    onBottomPassedReverse: function () {
+                        $('[data-action=fixedHeader]').transition('fade out');
+                    }
+                });
 
-      // create sidebar and attach to menu open
-      $('.ui.sidebar')
-        .sidebar('attach events', '.toc.item');
-      //  });
+            // create sidebar and attach to menu open
+            $('.ui.sidebar')
+                .sidebar('attach events', '.toc.item');
+            //  });
 
-      // TODO: initial tabs, to be integrated into JS framework
-      $('.menu .item')
-        .tab();
+            // TODO: initial tabs, to be integrated into JS framework
+            $('.menu .item')
+                .tab();
+        }
     }
-  }
 });
