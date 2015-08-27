c4dfdf757721abe0e373a15620d08ea91d5e13c9
田杰 <jefftian@hcdlearning.com>
2015-08-27 19:38:53 +0800

    #BRID-95: Sign up logic.

--- client/www/js/page/register/main.js
+++ client/www/js/page/register/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('register', [
+angular.module('signIn', [
     'ui.router',
     'ng.utils'
 ])
@@ -12,7 +12,123 @@ angular.module('register', [
     ])
     .run(function () {
 
-    });
+    })
+    .controller('SignUpCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+        $scope.signUpData = {
+            mobile: '',
+            captcha: '',
+            verificationCode: '',
+            password: ''
+        };
+
+        var validChineseMobileNumberPattern = '^(?:(0?86)|[\\(（](0?86)[\\)）])?-?(13\\d|15\\d|14[57]|17\\d|18\\d)(\\d{8})$';
+
+        var countDownInterval = 60;
+        var countDown = countDownInterval;
+
+        function updateButtonText(text) {
+            $scope.verificationCodeButtonText = text;
+        }
+
+        function initButtonText() {
+            updateButtonText('获取手机验证码');
+            $scope.allowGetCode = true;
+        }
+
+        function refreshButtonText() {
+            updateButtonText(countDown + ' 秒后重新获取...');
+            $scope.allowGetCode = false;
+        }
+
+        function pollUpdateButtonText() {
+            refreshButtonText();
+
+            if (countDown > 0) {
+                $timeout(function () {
+                    countDown--;
+                    pollUpdateButtonText();
+                }, 1000);
+            } else {
+                countDown = countDownInterval;
+                initButtonText();
+            }
+        }
+
+        initButtonText();
+
+        var fields = {
+            mobile: {
+                identifier: 'mobile',
+                rules: [
+                    {
+                        type: 'regExp[' + validChineseMobileNumberPattern + ']',
+                        prompt: '请输入有效的手机号码'
+                    }
+                ]
+            },
+
+            captcha: {
+                identifier: 'captcha',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: '请输入验证码'
+                    }
+                ]
+            }
+        };
+
+        var templates = {
+            error: function (errors) {
+                var html = '<ul class="list">';
+                $.each(errors, function (index, value) {
+                    html += '<li>' + value + '</li>';
+                });
+                html += '</ul><i class="large remove circle icon"></i>';
+
+                return $(html);
+            }
+        };
+
+        function getSignUpForm() {
+            return $('form.b-sign-up');
+        }
+
+        function partiallyValidateSignUpForm() {
+            var $form = getSignUpForm();
+
+            $form.form({
+                fields: fields,
+                templates: templates
+            });
+
+            $form.form('validate form');
+
+            return $form;
+        }
+
+        $scope.isSignUpFormPartiallyValid = function () {
+            //return partiallyValidateSignUpForm().form('is valid');
+            return $scope.signUpData.mobile && $scope.signUpData.mobile.match(new RegExp(validChineseMobileNumberPattern)) && $scope.signUpData.captcha;
+        };
+
+        $scope.isSignUpFormFullyValid = function () {
+            return $scope.isSignUpFormPartiallyValid() && $scope.signUpData.verificationCode && $scope.signUpData.password;
+        };
+
+        $scope.getVerificationCode = function () {
+            partiallyValidateSignUpForm();
+
+            if ($scope.isSignUpFormPartiallyValid()) {
+                pollUpdateButtonText();
+            }
+        };
+
+        $scope.trySignUp = function () {
+            alert('submitted');
+        };
+    }])
+;
 
 // TODO: integrated into JS framework
 (function () {
@@ -55,6 +171,16 @@ angular.module('register', [
                         {
                             type: 'empty',
                             prompt: '请输入验证码'
+                        }
+                    ]
+                },
+
+                verificationCode: {
+                    identifier: 'verificationCode',
+                    rules: [
+                        {
+                            type: 'empty',
+                            prompt: '请输入手机验证码'
                         }
                     ]
                 }
