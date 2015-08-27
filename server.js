d249a6de387b8f6062b1cfc932d15bf81e2680ce
田杰 <jefftian@hcdlearning.com>
2015-08-27 17:30:26 +0800

    #BRID-95: Reset password page.

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ var server = express();
 var ejs = require('ejs');
 
 // Set the view engine to ejs, but specify file type with ".html"
-server.engine('html', require('ejs').renderFile);
+server.engine('html', ejs.renderFile);
 server.set('view engine', 'html');
+
+server.get('/', function (req, res) {
+    res.render('index');
+});
 
 // Customize client file path
 server.set('views', __dirname + '/client/www');
@@ -20,6 +24,10 @@ server.get('/index', function (req, res) {
 
 server.get('/signin', function (req, res) {
     res.render('sign-in');
+});
+
+server.get('/reset-password', function (req, res) {
+    res.render('reset-password');
 });
 
 // Host & Port
