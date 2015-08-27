2c8a448d4cb055fd7b18a9290281c2b274ed5467
田杰 <jefftian@hcdlearning.com>
2015-08-27 14:35:41 +0800

    #BRID-95: Style form's error message.

--- client/www/js/page/register/main.js
+++ client/www/js/page/register/main.js
@@ -23,4 +23,54 @@ angular.module('register', [
             //context: '.b-signin-wide .menu .item',
             //history: true
         });
+
+    $('.ui.form')
+        .form({
+            fields: {
+                mobile: {
+                    identifier: 'mobile',
+                    rules: [
+                        {
+                            type: 'regExp[/^(?:(0?86)|[\\(（](0?86)[\\)）])?-?(13\\d|15\\d|14[57]|17\\d|18\\d)(\\d{8})$/]',
+                            prompt: '请输入有效的手机号码'
+                        }
+                    ]
+                },
+
+                password: {
+                    identifier: 'password',
+                    rules: [
+                        {
+                            type: 'empty',
+                            prompt: '密码不能为空'
+                        }
+                    ]
+                },
+
+                captcha: {
+                    identifier: 'captcha',
+                    rules: [
+                        {
+                            type: 'empty',
+                            prompt: '请输入验证码'
+                        }
+                    ]
+                }
+            },
+            templates: {
+                error: function (errors) {
+                    var html = '<ul class="list">';
+                    $.each(errors, function (index, value) {
+                        html += '<li>' + value + '</li>';
+                    });
+                    html += '</ul><i class="large remove circle icon"></i>';
+
+                    return $(html);
+                }
+            }
+        });
+
+    $('.ui.form').on('click', '.remove.circle.icon', function () {
+        $(this).parent().remove();
+    });
 })();
