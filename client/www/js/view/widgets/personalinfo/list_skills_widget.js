837ecc9326c1fe38ce29e95ed1c6ed9ad43593df
happybridge14 <happybridge14@163.com>
2015-08-27 18:04:27 +0800

    Add testing page for widgets on personal info page

--- client/www/js/view/widgets/personalinfo/list_skills_widget.js
+++ client/www/js/view/widgets/personalinfo/list_skills_widget.js
@@ -5,7 +5,7 @@ define([
   "bplus-ui/view/widgets/personalinfo/list_widget_base",
   "bplus-ui/view/widgets/common/tag",
   "text!./list_skills_widget.html"
-], function(when, $, angular, BaseClass, tag, template) {
+], function(when, $, angular, BaseClass, Tag, template) {
 
   var ListSkill = function() {
     BaseClass.call(this);
@@ -16,24 +16,36 @@ define([
   ListSkill.prototype.start = function(agModel) {
     var me = this;
     agModel.controller("skillscontroller", function($scope){
-     $scope.name = "myname"; 
      $scope.ENUM_STATUS = me.ENUM_STATUS;
      $scope.property = me.property;
+     var tags = [];
+     $scope.skill = me.data = {
+       name: "",
+       description: "",
+       tags: tags
+     };
+     $scope.taginput = {
+         candidates: [],
+         data: tags
+       }
      $scope.submit = function() {
        $scope.clicked = true;
        //TODO
        //submit function
      };
      $scope.cancel = function() {
+       debugger;
        //TODO
        //cancel function
      };
-    }).directive("bplusskills", function(){
+    })
+    .directive("bplusskills", function(){
       return {
         restrict: 'E',
-        template: template
-      }
+        template: template,
+      };
     })
+    new Tag().start(agModel);
   }
   
   return ListSkill;
