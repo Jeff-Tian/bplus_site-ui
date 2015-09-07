888e58418fedfe3e28fb991fb53da362154c2b4d
kimi.zhang <kimi.zhang@ef.com>
2015-09-07 16:39:22 +0800

    demo

--- client/www/js/page/profile/main.js
+++ client/www/js/page/profile/main.js
@@ -42,3 +42,65 @@
         }
     });
 })();
+
+var require = {
+    "waitSeconds": 0,
+    "baseUrl": "bower",
+    "packages": [{
+        "name": "jquery",
+        "location": "jquery/dist",
+        "main": "jquery"
+    }, {
+        "name": "when",
+        "location": "when",
+        "main": "when"
+    }, {
+        "name": "less",
+        "location": "less/dist",
+        "main": "less"
+    }, {
+        "name": "bplus-ui",
+        "location": "../js"
+    }],
+    "paths": {
+        "text": "requirejs-plugins/lib/text",
+        "image": "requirejs-plugins/src/image",
+        "json": "requirejs-plugins/src/json",
+        "propertyParser": "requirejs-plugins/src/propertyParser",
+        "angular": "angularjs/angular",
+        "angular-route": "angular-route/angular-route",
+        'domReady': 'requirejs-domready/domReady',
+        "semantic": "semantic-ui/dist/semantic",
+        "bplus-mock": "../mock"
+    },
+    "shim": {
+        'angular': {
+            exports: 'angular'
+        },
+        "angular-route":{
+            deps: ["angular"],
+            exports:"angular-route"
+        },
+        'semantic': {
+            deps: ["jquery"]
+        }
+    },
+    "deps": ["require", "less","semantic", "angular-route"],
+    "callback": function(require, less) {
+        require([
+            "angular",
+            "bplus-ui/page/profile/widget/banner/main"
+        ], function(
+            angular,
+            banner
+        ) {
+            var documentMudule = angular.module('docModule', []);
+
+            banner(documentMudule);
+
+            angular.element(window.document).ready(function() {
+                angular.bootstrap(window.document, ['docModule']);
+            });
+        })
+    }
+};
