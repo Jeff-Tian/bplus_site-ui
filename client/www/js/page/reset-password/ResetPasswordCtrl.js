fa73a001f90c9942dae1ce0d962b3fa605d34093
田杰 <jefftian@hcdlearning.com>
2015-09-07 19:34:59 +0800

    #BRID-96: delete unused code.

--- client/www/js/page/reset-password/ResetPasswordCtrl.js
+++ client/www/js/page/reset-password/ResetPasswordCtrl.js
@@ -3,42 +3,17 @@
         $scope.formCtrl = {};
 
         $scope.resetPassword = function () {
-            $http.post('/service-proxy/member/resetPassword', {})
+            $http.post('/service-proxy/member/resetPassword', $scope.formCtrl.getFormData())
                 .success(function (res) {
                     if (res.isSuccess) {
                         $('.reset.shape').shape('flip over');
                     } else {
-                        FormValidation.handleFormError($form, res.message);
+                        $scope.formCtrl.handleFormError(res.message);
                     }
-                }).error(FormValidation.delegateHandleFormError($form));
+                }).error($scope.formCtrl.handleFormError);
         };
 
         $('.reset.shape').shape();
-        var $form = $('.reset-password-side').find('form');
-        
-        $form.form({
-            on: 'blur',
-            inline: true,
-            fields: {
-                email: {
-                    identifier: 'email',
-                    rules: [{
-                        type: 'empty',
-                        prompt: '请填写邮箱地址'
-                    }, {
-                        type: 'email',
-                        prompt: '请填写有效的邮箱地址'
-                    }]
-                },
-                captcha: {
-                    identifier: 'captcha',
-                    rules: [{
-                        type: 'empty',
-                        prompt: '请填写验证码'
-                    }]
-                }
-            }
-        });
     };
 
     exports.ResetPasswordCtrl.$inject = ['$scope', '$element', '$http', 'FormValidation'];
