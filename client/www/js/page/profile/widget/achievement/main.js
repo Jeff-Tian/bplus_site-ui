b975f7b66ec52be32f298887b26dc2b6c734f130
kimi.zhang <kimi.zhang@ef.com>
2015-09-10 03:03:21 +0800

    achievement face

--- client/www/js/page/profile/widget/achievement/main.js
+++ client/www/js/page/profile/widget/achievement/main.js
@@ -3,28 +3,57 @@ define([
 ], function (
 
 ) {
+    function progressFace(val) {
+        val = parseInt(val);
+        var progress = document.getElementById('b-profile-face-progress'),
+            progress2 = document.getElementById('b-profile-face-progress2'),
+            step = 1,
+            space = parseInt(1000 / val),
+            maxV1 = val >= 50 ? 50 : (val <= 0 ? 0 : val),
+            maxV2 = (val > 50 && val < 100) ? val : 100;
+        function fnProgress() {
+            var v = parseFloat(progress.getAttribute('value'));
+            if (v < maxV1) {
+                progress.setAttribute('value', v + step);
+                setTimeout(fnProgress, space);
+            } else if (val > 50) {
+                fnProgress2();
+            }
+        }
+        function fnProgress2() {
+            var v = parseFloat(progress2.getAttribute('value'));
+            if (v < maxV2) {
+                progress2.setAttribute('value', v + step);
+                setTimeout(fnProgress2, space);
+            }
+        }
+        fnProgress();
+    }
+
     return function (agModule) {
-        agModule.controller('achievement', ['$scope', function ($scope) {
-            $scope.face = '/img/profile/icon_profile_picture_male_big.png';
-            $scope.pics = [
-                {image: '/img/profile/achievement-img.jpg'},
-                {image: '/img/profile/achievement-img.jpg'},
-                {image: '/img/profile/achievement-img.jpg'},
-                {image: '/img/profile/achievement-img.jpg'},
-                {image: '/img/profile/achievement-img.jpg'}
-            ];
-            $scope.level = '9';
-            $scope.scroe = 99;
-            $scope.list = [{
-                title: '分享信息到朋友圈',
-                scroe: '+3'
-            }, {
-                title: '邀请朋友加入',
-                scroe: '+5'
-            }, {
-                title: '注册成功',
-                scroe: '+5'
-            }];
+        agModule.controller('achievement', ['$scope', '$http', function ($scope, $http) {
+            $http.get('/mock/profile-achievement.json').success( function (data) {
+                var isProgress = false;
+                (function () {
+                    var list = (data && (data.list instanceof Array)) ? data.list : [];
+                    for (var i = 0, len = list.length; i < len; i++) {
+                        var item = list[i];
+                        if (('score' in item) && !/^[\+\-]/gim.test(item.score.toString())) {
+                            item.score = '+' + item.score;
+                        }
+                    }
+                })();
+                if (data && ('score' in data)) {
+                    data.score = '+' + data.score;
+                }
+                if (typeof(data.progress) == 'number' && (!$scope.data || ($scope.data.progress != data.progress))) {
+                    isProgress = true;
+                }
+                $scope.data = data;
+                if (isProgress) {
+                    progressFace($scope.data.progress);
+                }
+            });
         }]);
     }
 });
--- client/www/js/page/profile/widget/growing/main.js
+++ client/www/js/page/profile/widget/growing/main.js
@@ -4,21 +4,10 @@ define([
 
 ) {
     return function (agModule) {
-        agModule.controller('growing', ['$scope', function ($scope) {
-            $scope.list = [{
-                title: '第一届Bridge+全国商业实战大赛 华东赛区 特等奖'
-            }, {
-                title: '第二届Bridge+全国商业实战大赛 复赛 优胜奖'
-            }, {
-                title: '第二届Bridge+全国商业实战大赛 决赛 第一名'
-            }];
-            $scope.buttons = [{
-                title: '查看历次比赛成绩'
-            }, {
-                title: '下载比赛结果报告'
-            }, {
-                title: '下载比赛获奖证书'
-            }];
+        agModule.controller('growing', ['$scope', '$http', function ($scope, $http) {
+            $http.get('/mock/profile-growing.json').success( function (data) {
+                $scope.data = data;
+            });
         }]);
     }
 });
