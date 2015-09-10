7e1dbe454f3c993f518d5ef7792d86e591b0edca
kimi.zhang <kimi.zhang@ef.com>
2015-09-11 02:28:54 +0800

    profile face

--- client/www/js/page/profile/main.js
+++ client/www/js/page/profile/main.js
@@ -1,49 +1,5 @@
-'use strict';
-
-// TODO: integrated into JS framework
-(function() {
-    //$(document)
-    //  .ready(function() {
-    // fix header when passed
-    $('.b-masthead')
-        .visibility({
-            once: false,
-            onBottomPassed: function() {
-                $('[data-action=fixedHeader]').transition('fade in');
-            },
-            onBottomPassedReverse: function() {
-                $('[data-action=fixedHeader]').transition('fade out');
-            }
-    });
-
-    // create sidebar and attach to menu open
-    $('.ui.sidebar')
-        .sidebar('attach events', '.toc.item');
-    //  });
-
-    // TODO: initial tabs, to be integrated into JS framework
-    $('.menu .item')
-        .tab();
-
-    // TODO: initial carousel, to be integrated into JS framework
-    $('[data-action=carousel]').owlCarousel({
-        singleItem: true,
-        lazyLoad: true,
-        loop: true,
-        autoPlay: true,
-        autoplayTimeout: 10000,
-        navigation: true,
-        navigationText: ['<i class="angle left icon"></i>', '<i class="angle right icon"></i>'],
-        afterInit: function() {
-            console.log("carousel init");
-        },
-        afterMove: function() {
-            console.log("carousel move");
-        }
-    });
-})();
-
-var require = {
+require.config({
+    "context": "bplus",
     "waitSeconds": 0,
     "baseUrl": "bower",
     "packages": [{
@@ -84,75 +40,85 @@ var require = {
         'semantic': {
             deps: ["jquery"]
         }
-    },
-    "deps": ["require", "less","semantic", "angular-route"],
-    "callback": function(require, less) {
-        require([
-            "angular",
-
-            "bplus-ui/page/profile/widget/banner/main",
-            "bplus-ui/page/profile/widget/achievement/main",
-            "bplus-ui/page/profile/widget/growing/main",
-
-            "bplus-ui/view/widgets/personalinfo/list_skills_widget",
-            "bplus-ui/view/widgets/personalinfo/list_personalinfo_widget",
-            "bplus-ui/view/widgets/personalinfo/list_educationbackground_widget",
-            "bplus-ui/view/widgets/personalinfo/list_clubexperience_widget",
-            "bplus-ui/view/widgets/personalinfo/list_widget_container",
-            "text!bplus-ui/view/widgets/personalinfo/list_personalinfo_container.html",
-            "text!bplus-ui/view/widgets/personalinfo/list_skills_container.html",
-            "text!bplus-ui/view/widgets/personalinfo/list_educationbackground_container.html",
-            "text!bplus-ui/view/widgets/personalinfo/list_clubexperience_container.html"
-        ], function(
-            angular,
-
-            banner,
-            achievement,
-            growing,
-
-            skills,
-            personalinfo,
-            educationbackground,
-            club,
-            container,
-            personalinfoContainerTemplate,
-            skillsContainerTemplate,
-            eduContainerTemplate,
-            clubContainerTemplate
-        ) {
-            var documentMudule = angular.module('docModule', []);
-
-            (function (agModule) {
-                banner(agModule);
-                achievement(agModule);
-                growing(agModule);
-
-                //  var skillInstance = new skills();
-                //  skillInstance.start(agModule);
-                var instance = new container();
-                instance.start(agModule, "bpluspersonalinfooverall", personalinfoContainerTemplate);
-                var instance1 = new container();
-                instance1.start(agModule, "bplusskillsoverall", skillsContainerTemplate);
-                var instance2 = new container();
-                instance2.start(agModule, "bpluseducationbackgroundall", eduContainerTemplate);
-                var instance3 = new container();
-                instance3.start(agModule, "bplusclubexperienceall", clubContainerTemplate);
-
-                var instancePersonalInfo = new personalinfo();
-                instancePersonalInfo.start(agModule);
-                var instanceSkills = new skills();
-                instanceSkills.start(agModule);
-                var instanceEdu = new educationbackground();
-                instanceEdu.start(agModule);
-                var instanceClub = new club();
-                instanceClub.start(agModule);
-                //  var instance1 = new skills();
-                //  instance1.start(agModule);
-            })(documentMudule);
-
-            angular.element(window.document).ready(function() {
-                angular.bootstrap(window.document, ['docModule']);
-            });
-        })
     }
-};
+});
+
+require.config({"context": "bplus"})([
+    "require",
+    "less",
+    "semantic",
+    "angular-route"
+], function(
+    pRequire,
+    less,
+    semantic,
+    agRoute
+) {
+    pRequire([
+        "angular",
+
+        "bplus-ui/page/profile/widget/banner/main",
+        "bplus-ui/page/profile/widget/achievement/main",
+        "bplus-ui/page/profile/widget/growing/main",
+
+        "bplus-ui/view/widgets/personalinfo/list_skills_widget",
+        "bplus-ui/view/widgets/personalinfo/list_personalinfo_widget",
+        "bplus-ui/view/widgets/personalinfo/list_educationbackground_widget",
+        "bplus-ui/view/widgets/personalinfo/list_clubexperience_widget",
+        "bplus-ui/view/widgets/personalinfo/list_widget_container",
+        "text!bplus-ui/view/widgets/personalinfo/list_personalinfo_container.html",
+        "text!bplus-ui/view/widgets/personalinfo/list_skills_container.html",
+        "text!bplus-ui/view/widgets/personalinfo/list_educationbackground_container.html",
+        "text!bplus-ui/view/widgets/personalinfo/list_clubexperience_container.html"
+    ], function(
+        angular,
+
+        banner,
+        achievement,
+        growing,
+
+        skills,
+        personalinfo,
+        educationbackground,
+        club,
+        container,
+        personalinfoContainerTemplate,
+        skillsContainerTemplate,
+        eduContainerTemplate,
+        clubContainerTemplate
+    ) {
+        var documentMudule = angular.module('docModule', []);
+
+        (function (agModule) {
+            banner(agModule);
+            achievement(agModule);
+            growing(agModule);
+
+            //  var skillInstance = new skills();
+            //  skillInstance.start(agModule);
+            var instance = new container();
+            instance.start(agModule, "bpluspersonalinfooverall", personalinfoContainerTemplate);
+            var instance1 = new container();
+            instance1.start(agModule, "bplusskillsoverall", skillsContainerTemplate);
+            var instance2 = new container();
+            instance2.start(agModule, "bpluseducationbackgroundall", eduContainerTemplate);
+            var instance3 = new container();
+            instance3.start(agModule, "bplusclubexperienceall", clubContainerTemplate);
+
+            var instancePersonalInfo = new personalinfo();
+            instancePersonalInfo.start(agModule);
+            var instanceSkills = new skills();
+            instanceSkills.start(agModule);
+            var instanceEdu = new educationbackground();
+            instanceEdu.start(agModule);
+            var instanceClub = new club();
+            instanceClub.start(agModule);
+            //  var instance1 = new skills();
+            //  instance1.start(agModule);
+        })(documentMudule);
+
+        angular.element(window.document).ready(function() {
+            angular.bootstrap(window.document, ['docModule']);
+        });
+    });
+});
--- client/www/js/page/profile/widget/achievement/main.js
+++ client/www/js/page/profile/widget/achievement/main.js
@@ -5,36 +5,34 @@ define([
 ) {
     function progressFace(val) {
         val = parseInt(val);
-        var progress = document.getElementById('b-profile-face-progress'),
-            progress2 = document.getElementById('b-profile-face-progress2'),
+        var progressLeft = document.getElementById('progressLeft'),
+            progressRight = document.getElementById('progressRight'),
             step = 1,
             space = parseInt(1000 / val),
-            maxV1 = val >= 50 ? 50 : (val <= 0 ? 0 : val),
-            maxV2 = (val > 50 && val < 100) ? val : 100;
-        function fnProgress() {
-            var v = parseFloat(progress.getAttribute('value'));
-            if (v < maxV1) {
-                progress.setAttribute('value', v + step);
-                setTimeout(fnProgress, space);
+            maxRight = val >= 50 ? 180 : (val <= 0 ? 0 : val * 3.6),
+            maxLeft = (val > 50 && val < 100) ? (val - 50) * 3.6 : 180;
+        function circleRight() {
+            var deg = parseInt(progressRight.style.transform.toString().match(/rotate\((\d+)deg\)/im)[1]);
+            if (deg < maxRight) {
+                progressRight.style.transform = 'rotate(' + (deg + 1) + 'deg)';
+                setTimeout(circleRight, space);
             } else if (val > 50) {
-                fnProgress2();
+                circleLeft();
             }
         }
-        function fnProgress2() {
-            var v = parseFloat(progress2.getAttribute('value'));
-            if (v < maxV2) {
-                progress2.setAttribute('value', v + step);
-                setTimeout(fnProgress2, space);
+        function circleLeft() {
+            var deg = parseInt(progressLeft.style.transform.toString().match(/rotate\((\d+)deg\)/im)[1]);
+            if (deg < maxLeft) {
+                progressLeft.style.transform = 'rotate(' + (deg + 1) + 'deg)';
+                setTimeout(circleLeft, space);
             }
         }
-        progress.setAttribute('value', 0);
-        progress2.setAttribute('value', 50);
-        fnProgress();
+        circleRight();
     }
 
     return function (agModule) {
         agModule.controller('achievement', ['$scope', '$http', function ($scope, $http) {
-            $scope.classNameFaceEdit = 'edit';
+            $scope.classNameFaceEdit = '';
             $http.get('/mock/profile-achievement.json').success( function (data) {
                 if (!data) { return; }
 
