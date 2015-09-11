fa5a1c218fe51fb9e2e25ada1abe665ebed78e93
kimi.zhang <kimi.zhang@ef.com>
2015-09-12 02:12:52 +0800

    profile

--- client/www/js/page/profile/main.js
+++ client/www/js/page/profile/main.js
@@ -65,11 +65,17 @@ require.config({"context": "bplus"})([
         "bplus-ui/view/widgets/personalinfo/list_personalinfo_widget",
         "bplus-ui/view/widgets/personalinfo/list_educationbackground_widget",
         "bplus-ui/view/widgets/personalinfo/list_clubexperience_widget",
+        "bplus-ui/view/widgets/personalinfo/list_workingexperience_widget",
+        "bplus-ui/view/widgets/personalinfo/list_award_widget",
+        "bplus-ui/view/widgets/personalinfo/list_language_widget",
         "bplus-ui/view/widgets/personalinfo/list_widget_container",
         "text!bplus-ui/view/widgets/personalinfo/list_personalinfo_container.html",
         "text!bplus-ui/view/widgets/personalinfo/list_skills_container.html",
         "text!bplus-ui/view/widgets/personalinfo/list_educationbackground_container.html",
-        "text!bplus-ui/view/widgets/personalinfo/list_clubexperience_container.html"
+        "text!bplus-ui/view/widgets/personalinfo/list_clubexperience_container.html",
+        "text!bplus-ui/view/widgets/personalinfo/list_workingexperience_container.html",
+        "text!bplus-ui/view/widgets/personalinfo/list_award_container.html",
+        "text!bplus-ui/view/widgets/personalinfo/list_language_container.html"
     ], function(
         angular,
 
@@ -81,11 +87,17 @@ require.config({"context": "bplus"})([
         personalinfo,
         educationbackground,
         club,
+        work,
+        award,
+        language,
         container,
         personalinfoContainerTemplate,
         skillsContainerTemplate,
         eduContainerTemplate,
-        clubContainerTemplate
+        clubContainerTemplate,
+        workContainerTemplate,
+        awardContainerTemplate,
+        languageContainerTemplate
     ) {
         var documentMudule = angular.module('docModule', []);
 
@@ -104,6 +116,12 @@ require.config({"context": "bplus"})([
             instance2.start(agModule, "bpluseducationbackgroundall", eduContainerTemplate);
             var instance3 = new container();
             instance3.start(agModule, "bplusclubexperienceall", clubContainerTemplate);
+            var instance4 = new container();
+            instance4.start(agModule, "bplusworkexperienceall", workContainerTemplate);
+            var instance5 = new container();
+            instance5.start(agModule, "bplusawardall", awardContainerTemplate);
+            var instance6 = new container();
+            instance6.start(agModule, "bpluslanguageall", languageContainerTemplate);
 
             var instancePersonalInfo = new personalinfo();
             instancePersonalInfo.start(agModule);
@@ -113,6 +131,12 @@ require.config({"context": "bplus"})([
             instanceEdu.start(agModule);
             var instanceClub = new club();
             instanceClub.start(agModule);
+            var instanceWork = new work();
+            instanceWork.start(agModule);
+            var instanceAward = new award();
+            instanceAward.start(agModule);
+            var instanceLanguage = new language();
+            instanceLanguage.start(agModule);
             //  var instance1 = new skills();
             //  instance1.start(agModule);
         })(documentMudule);
