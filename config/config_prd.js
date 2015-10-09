3bed5d8c52e075f022f53c5a373e90e920896ce5
happybridge14 <happybridge14@163.com>
2015-10-09 14:33:07 +0800

    Update some string. Add config_prd

---
+++ config/config_prd.js
@@ -0,0 +1,58 @@
+(function () {
+    var bplusServiceParams = (typeof require === "function") ? require("./config_bplusService") : {};
+    var config = {
+        sms: {
+            host: 'live.service.hcd.com',
+            port: '10002',
+            code: "BP_S1",
+            enabled: true
+        },
+        mail: {
+            host: 'live.service.hcd.com',
+            port: '10002',
+            code: 'BP_M1',
+            verificationCode: 'BP_M2'
+        },
+        captcha: {
+            host: 'live.captcha.service.hcdlearning.com',
+            port: '10001'
+        },
+        sso: {
+            host: 'live.service.hcd.com',
+            port: '10086'
+        },
+        bplusService: {
+            host: 'live.service.hcd.com',
+            port: '12345'
+        },
+        wechat: {
+            host: 'live.service.hcd.com',
+            port: '10101'
+        },
+        "bplusServiceParams": bplusServiceParams,
+        "logger": {
+            "levels": {
+                "[all]": "DEBUG"
+            },
+            "appenders": [{
+                "type": "console"
+            }, {
+                "type": "log4js-cassandra",
+                "nodes": ["live.cass01.server", "live.cass02.server"],
+                "username": "cassandra",
+                "password": "HCDhcd@123",
+                "keyspace": "logdb",
+                "table": "log",
+                "appName": "BridgePlus"
+            }]
+        },
+        applicationId: '2b33cf2c-e5dd-4e82-8687-d3fe099a3504'
+    };
+
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = config;
+    } else if (typeof angular !== 'undefined') {
+        angular.bplus = angular.bplus || {};
+        angular.bplus.config = config;
+    }
+})();
