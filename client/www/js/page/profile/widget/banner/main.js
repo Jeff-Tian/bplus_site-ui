b114d28ddd2f6d4437699e550a7b9d354b062946
田杰 <jefftian@hcdlearning.com>
2015-10-14 15:54:01 +0800

    Fix comma missing issue.

--- client/www/js/page/profile/widget/banner/main.js
+++ client/www/js/page/profile/widget/banner/main.js
@@ -1,28 +1,27 @@
 define([
     "angular"
-], function (
-    angular
-) {
+], function (angular) {
     var SERVICE_MEMBER = "memberExt";
     var bgBanner = [
         'img/profile/banner.1.jpg',
         'img/profile/banner.2.jpg',
         'img/profile/banner.3.jpg'
     ];
-    var createImageUrl = function(imagePath) {
-        return angular.bplus.config.cdn.normal + imagePath + "?" + angular.bplus.config.cdn.version; 
-    }
+    var createImageUrl = function (imagePath) {
+        return angular.bplus.config.cdn.normal + imagePath + "?" + angular.bplus.config.cdn.version;
+    };
+
     return function (agModule) {
         agModule.controller('banner', ['$scope', 'personalinfoService', function ($scope, model) {
-            model.getData(SERVICE_MEMBER).then(function(data) {
+            model.getData(SERVICE_MEMBER).then(function (data) {
                 var memberData = data[0];
-                var imagePath = memberData.userBackground ? memberData.userBackground : bgBanner[0]; 
+                var imagePath = memberData.userBackground ? memberData.userBackground : bgBanner[0];
                 $scope.imageBanner = createImageUrl(imagePath);
                 $scope.hoverBtn = '';
 
             });
             $scope.switchBanner = function (i) {
-                var imagePath = bgBanner[i]; 
+                var imagePath = bgBanner[i];
                 $scope.imageBanner = createImageUrl(imagePath);
                 model.updateData(SERVICE_MEMBER, {
                     userBackground: imagePath
