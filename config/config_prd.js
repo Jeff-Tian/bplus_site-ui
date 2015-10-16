981c10dd07e878702fb15cfcc6f53685c87e4a7e
henry <happybridge14@163.com>
2015-10-16 11:25:29 +0800

    Add date as version

--- config/config_prd.js
+++ config/config_prd.js
@@ -4,6 +4,6 @@ var config = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'config_prd.json
 
 config.captcha.host = (process.env.DATACENTER ? process.env.DATACENTER + '-' : '') + config.captcha.host;
 config.bplusServiceParams = require("./config_bplusService");
-config.cdn.version = config.version;
+config.cdn.version = config.version + "_" + config.date;
 
 module.exports = config;
