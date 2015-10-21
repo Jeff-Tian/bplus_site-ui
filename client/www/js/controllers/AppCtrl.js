34dfc7d81c0a5f065a52b750ea5f5a8220ec414b
henry <happybridge14@163.com>
2015-10-21 17:41:20 +0800

    Fix some bugs.

--- client/www/js/controllers/AppCtrl.js
+++ client/www/js/controllers/AppCtrl.js
@@ -60,6 +60,8 @@
         $scope.hash = window.location.hash;
         window.addEventListener('load', locationHashChanged);
         window.addEventListener('hashchange', locationHashChanged);
+
+        msgBus.onMsg(msgBus.events.profile.updated, $scope, $scope.fetchProfile);
     };
 
     exports.AppCtrl.$inject = ['FormValidation', '$scope', 'service', 'MessageStore', 'msgBus', '$translate'];
