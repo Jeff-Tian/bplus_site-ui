f9cf18661bc456c121d904e0f9f2e212557eb0ab
田杰 <jefftian@hcdlearning.com>
2015-10-28 11:35:05 +0800

    Log options when proxy error.

--- serviceProxy/proxy.js
+++ serviceProxy/proxy.js
@@ -88,7 +88,7 @@ function advancedProxy(req, res, next, settings) {
     });
 
     request.on('error', function (err) {
-        req.dualLogError('Error met in this request:');
+        req.dualLogError('Error met in this request: ' + JSON.stringify(options));
         req.dualLogError(request);
 
         next();
