65d5023e62ed40cdd079afec9aaf5b71fb120853
田杰 <jefftian@hcdlearning.com>
2015-11-03 16:10:59 +0800

    Fix config.

--- config/index.js
+++ config/index.js
@@ -1,9 +1,11 @@
-if (process.env.NODE_ENV === 'dev') {
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'dev') {
     module.exports = require('./config_dev.js');
 } else if (process.env.NODE_ENV === 'prd') {
-    if (!process.env.RUN_FROM === 'local') {
+    if (!(process.env.RUN_FROM === 'local')) {
         module.exports = require('./config_prd.js');
     } else {
         module.exports = require('./config_dev.js');
     }
+} else {
+    module.exports = require('./config_prd.js');
 }
