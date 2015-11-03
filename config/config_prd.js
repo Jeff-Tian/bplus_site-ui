c50fbc30fc3b3ef9579057d87e0468895dbf44f7
henry huang <happybridge14@163.com>
2015-11-03 15:28:23 +0800

    Update upload service config values

--- config/config_prd.js
+++ config/config_prd.js
@@ -5,9 +5,9 @@ var config = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'config_prd.json
 if (process.env.DATACENTER) {
     config.captcha.host = process.env.DATACENTER + '-' + config.captcha.host;
     config.payment.public.host = process.env.DATACENTER + '-' + config.payment.public.host;
+    config.service_upload = "//" + process.env.DATACENTE + '-' + config.upload.public.host + ":" + config.upload.public.port;
 }
 config.bplusServiceParams = require("./config_bplusService");
 config.cdn.version = config.version + "_" + config.date;
-config.service_upload = "//" + config.upload.public.host + ":" + config.upload.public.port;
 
 module.exports = config;
