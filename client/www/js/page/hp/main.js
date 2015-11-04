22f79e07c9f57d3d86ae014816108a631a5e0b50
monsoul <monsoul@msn.com>
2015-11-04 10:37:44 +0800

    statement and youth mobile

---
+++ client/www/js/page/hp/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('bplus', [
+    'ng.utils',
+    'pascalprecht.translate'
+])
+    .config(angular.bplus.translate)
+    .config(angular.bplus.xhr)
+    .factory('translationLoader', angular.bplus.translationLoader)
+    .factory('service', angular.bplus.service)
+    .factory('FormValidation', angular.bplus.FormValidation || function () {
+        return {};
+    })
+    .factory('MessageStore', angular.bplus.MessageStore)
+    .controller('AppCtrl', angular.bplus.AppCtrl)
+;
+// TODO: integrated into JS framework
+(function() {
+    //$(document)
+    //  .ready(function() {
+    // fix header when passed
+    $('.hp-mastead').visibility({
+        once: false,
+        onBottomPassed: function() {
+            $('[data-action=fixedHeader]').transition('fade in');
+        },
+        onBottomPassedReverse: function() {
+            $('[data-action=fixedHeader]').transition('fade out');
+        }
+    });
+
+
+    // create sidebar and attach to menu open
+    $('.ui.sidebar').sidebar('attach events', '.toc.item');
+    //  });
+
+    // TODO: initial tabs, to be integrated into JS framework
+    //$('.menu .item').tab();
+})();
