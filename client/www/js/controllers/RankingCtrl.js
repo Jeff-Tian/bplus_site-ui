a3eae7a37488065df1a277c7a478eb50ff1702ae
henry huang <happybridge14@163.com>
2015-12-08 12:23:03 +0800

    Update wechat sharing part

--- client/www/js/controllers/RankingCtrl.js
+++ client/www/js/controllers/RankingCtrl.js
@@ -52,74 +52,13 @@
         return retArray;
     };
 
-    exports.RankingCtrl = function ($scope, service, queryParser, WechatWrapper) {
+    exports.RankingCtrl = function ($scope, service, queryParser) {
         $scope.option = OPTION_INIT;
         $scope.details = refreshDetails($scope.option);
         $scope.rankChange = function(option) {
             $scope.option = option;
             $scope.details = refreshDetails(option);
         };
-
-        $('.rank-wechat-share').popup({
-            on: 'hover',
-        });
-        service.post(angular.bplus.config.serviceUrls.wechatJsApiConfig, {
-            url: window.location.origin + window.location.pathname
-        }).then(function (result) {
-            var jsApiList = [
-                'onMenuShareAppMessage',
-                'onMenuShareTimeline'
-            ];
-
-            WechatWrapper.config({
-                debug: false,
-                appId: result.appId,
-                timestamp: String(result.timestamp),
-                nonceStr: String(result.noncestr),
-                signature: result.signature,
-                jsApiList: jsApiList
-            });
-
-            WechatWrapper.ready(function () {
-                WechatWrapper.checkJsApi({
-                    jsApiList: jsApiList,
-                    success: function (res) {
-                        var imageUrl = angular.bplus.config.cdn.normal + 'img/wechat/for_share.png?' + angular.bplus.config.cdn.version;
-                        if (imageUrl.indexOf('//') === 0) {
-                            imageUrl = 'http:' + imageUrl;
-                        } else {
-                            imageUrl = location.origin + imageUrl;
-                        }
-
-                        WechatWrapper.onMenuShareAppMessage({
-                            title: $rootScope.pageTitle,
-                            desc: $rootScope.pageDescription,
-                            link: location.href,
-                            imgUrl: imageUrl,
-                            trigger: function (res) {
-                                // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
-                                //alert('用户点击发送给朋友');
-                            },
-                            success: function (res) {
-                                //alert('已分享');
-                            },
-                            cancel: function (res) {
-                                //alert('已取消');
-                            },
-                            fail: function (res) {
-                                //alert(JSON.stringify(res));
-                            }
-                        });
-
-                        WechatWrapper.onMenuShareTimeline({
-                            title: $rootScope.pageTitle,
-                            link: location.href,
-                            imageUrl: imageUrl
-                        });
-                    }
-                });
-            });
-        });
     };
-    exports.RankingCtrl.$inject = ['$scope', 'service', 'queryParser', 'WechatWrapper'];
+    exports.RankingCtrl.$inject = ['$scope', 'service', 'queryParser'];
 })(angular.bplus = angular.bplus || {});
