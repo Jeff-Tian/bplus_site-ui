9cb0954ca28c32503e60df7a92cd84555f48dc4e
田杰 <jefftian@hcdlearning.com>
2015-12-16 17:58:03 +0800

    Exclude unecessary folders from rsync.

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('default', function (done) {
 gulp.task('assemble', function (done) {
     sh.mkdir('artifacts');
 
-    var command = 'rsync -rlptz --delete-after --exclude ".git" ./ artifacts';
+    var command = 'rsync -rlptz --delete-after --exclude-from "rsync-exclude.txt" ./. artifacts';
     console.log(command);
 
     sh.exec(command, done);
@@ -20,23 +20,27 @@ gulp.task('assemble', function (done) {
 gulp.task('build', function (done) {
     sh.cd('artifacts');
 
-    sh.exec('grunt release', function () {
-
-        sh.exec('cnpm prune --production', function () {
-
-            sh.cd('..');
-            done();
+    sh.exec('cnpm install', function () {
+        sh.exec('grunt release', function () {
+            sh.exec('cnpm prune --production', function () {
+                sh.cd('..');
+                done();
+            });
         });
     });
 });
 
 gulp.task('deploy-hz', function (done) {
     var remote = 'root@hz.liveq.net';
-    var command = 'rsync --compress --delete-after -ave ssh artifacts ' + remote + ':/var/lib/jenkins/workspace/Website_BridgePlus/.';
+    var command = 'rsync --compress --delete-after -ave ssh artifacts/. ' + remote + ':/var/lib/jenkins/workspace/Website_BridgePlus/.';
 
     console.log(command);
 
     sh.exec(command, done);
+});
+
+gulp.task('ab', function (done) {
+    runSequence('assemble', 'build', done);
 });
 
 gulp.task('hz', function (done) {
