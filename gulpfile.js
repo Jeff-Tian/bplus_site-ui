494ede9f9d08023c5a38fcaaf4f8f3f7ffcbfa02
田杰 <jefftian@hcdlearning.com>
2015-12-16 15:59:10 +0800

    Add gulp to do the deployment job from local machine.

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var sh = require('shelljs');
+var runSequence = require('run-sequence');
+
+gulp.task('default', function (done) {
+    console.log('hello');
+
+    done();
+});
+
+gulp.task('assemble', function (done) {
+    sh.mkdir('artifacts');
+
+    var command = 'rsync -rlptz --delete-after --exclude ".git" ./ artifacts';
+    console.log(command);
+
+    sh.exec(command, done);
+});
+
+gulp.task('build', function (done) {
+    sh.cd('artifacts');
+
+    sh.exec('grunt release', function () {
+
+        sh.exec('cnpm prune --production', function () {
+
+            sh.cd('..');
+            done();
+        });
+    });
+});
+
+gulp.task('deploy-hz', function (done) {
+    var remote = 'root@hz.liveq.net';
+    var command = 'rsync --compress --delete-after -ave ssh artifacts ' + remote + ':/var/lib/jenkins/workspace/Website_BridgePlus/.';
+
+    console.log(command);
+
+    sh.exec(command, done);
+});
+
+gulp.task('hz', function (done) {
+    runSequence('assemble', 'build', 'deploy-hz', done);
+});
