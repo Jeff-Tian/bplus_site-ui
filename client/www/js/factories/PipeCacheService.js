efa72087e11b7fab1ec3b25a762c75689e917aee
henry huang <happybridge14@163.com>
2016-01-04 18:50:45 +0800

    Update pipcacheservcie. remove hp.css

--- client/www/js/factories/PipeCacheService.js
+++ client/www/js/factories/PipeCacheService.js
@@ -1,18 +1,20 @@
 (function (exports) {
-    var todoArray = [];
+    var dataArray = [];
+    var dataObject= {};
     var todoIndex = 0;
-    var todoParam = null;
-    var todoPromise = null;
-    var externalParam = null;
-    var doneArray = [];
-    var doneDataArray = [];
-    var executingPromise = null;
-    var STATUS = {
+    var doingPromise = {};
+    var MACHINE_STATUS = {
         STOPPED: "stop",
         WORKING: "working",
         IDLE: "idle"
-    }
-    var status = STATUS.STOPPED;
+    };
+    var PROMISE_STATUS = {
+        PENDING: "pending",
+        WORKING: "working",
+        DONE: "done",
+        INVALID: "invalid"
+    };
+    var machine_status = MACHINE_STATUS.STOPPED;
     var KeyStructure = {
         url: "",
         params: ""
@@ -20,47 +22,79 @@
     var status = null;
     exports.PipeCacheService = function (service, $http) {
         var createPromise = function(paramWithURL) {
-            todoPromise = service.handleHttpPromise($http({
+            return service.handleHttpPromise($http({
                 url: paramWithURL.url,
                 params: paramWithURL.params,
                 method: "GET"
             }));
-            return todoPromise;
         }
-        var findNextPromise = function() {
-            todoParam = todoArray[todoIndex++];
-            var ret = null;
-            if (todoParam) {
-                ret = createPromise(nextParam);
-            } else {
-                todoParam = todoPromise = null;
+        var findNext = function() {
+            if (dataArray[todoIndex]) {
+                return dataArray[todoIndex++];
             }
-            return ret;
-        };
-        var promiseChain = function(promise) {
-            if (promise) {
-                return promise.then(function() {
-                    return promiseChain(findNextPromise());
-                })
-            }
-        };
-        var idle = function() {
-
         };
         var run = function() {
-            var currentParamObject = todoArray[todoIndex];
-            if (currentParamObject) {
-                status = STATUS.WORKING;
-            } else {
-                idle();
+            setInterval(function() {
+                if (Object.keys(doingPromise).length === 0) {
+                    var next = findNext();
+                    if (next) {
+                        getData(next);
+                    }
+                }
+            }, 500);
+        };
+        var addKeyWithStatus = function(paramWithURL, status) {
+            dataObject[JSON.stringify(paramWithURL)] = {
+                data: {},
+                index: dataArray.push(paramWithURL) - 1, 
+                status: status,
+                promise: null
+            };
+        };
+        var getData = function(paramWithURL) {
+            var data =dataObject[JSON.stringify(paramWithURL)];
+            doingPromise[JSON.stringify(paramWithURL)] = true;
+            var promise = createPromise(paramWithURL).then(function(retData) {
+                if (data) {
+                    data.status = (data.status === PROMISE_STATUS.INVALID) ? PROMISE_STATUS.INVALID : PROMISE_STATUS.DONE;
+                    data.promise = null;
+                    data.data = retData;
+                }
+                delete doingPromise[JSON.stringify(paramWithURL)];
+                return retData;
+            });
+            if (data) {
+                if (data.status === PROMISE_STATUS.PENDING) {
+                    data.status = PROMISE_STATUS.WORKING;
+                    data.promise = promise;
+                }
             }
+            return promise;
         };
         var pipeCacheService = {
-            add: function() {
-
+            add: function(paramWithURL) {
+                var data =dataObject[JSON.stringify(paramWithURL)]; 
+                if (!data) {
+                    addKeyWithStatus(paramWithURL, PROMISE_STATUS.PENDING);
+                }
             },
-            get: function() {
-
+            get: function(paramWithURL) {
+                var data = dataObject[JSON.stringify(paramWithURL)]; 
+                doingPromise[JSON.stringify(paramWithURL)] = true;
+                var index;
+                if (data) {
+                    if (data.status === PROMISE_STATUS.DONE) {
+                        data.status = PROMISE_STATUS.INVALID;
+                        delete doingPromise[JSON.stringify(paramWithURL)];
+                        return data.data;
+                    } else if(data.status === PROMISE_STATUS.WORKING) {
+                        return data.promise;
+                    } else {
+                        return getData(paramWithURL);
+                    }
+                } else {
+                    return getData(paramWithURL);
+                }
             }
         }
         //Start service
