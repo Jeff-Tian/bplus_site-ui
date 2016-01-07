e6e029e78a4335d946a1111aa5a2e9288157df46
田杰 <jefftian@hcdlearning.com>
2016-01-07 11:32:42 +0800

    Cdnify onlineStoreParentCtrl.js

--- client/www/js/controllers/online-store/onlineStoreParentCtrl.js
+++ client/www/js/controllers/online-store/onlineStoreParentCtrl.js
@@ -1,60 +1,48 @@
 (function (exports) {
-    var paymentDataStructure = {
-        title: "",
+    function cdnify(url) {
+        if (url[0] === '/') {
+            url = url.substr(1);
+        }
+
+        return angular.bplus.config.cdn.normal + url + '?' + angular.bplus.config.cdn.version;
+    }
+
+    var paymentList = [{
         offerId: "dcb33391-baad-4dd0-9c05-aa2e4f05c940",
-        price: "0.00",
-        details: [],
         forcusDetail: {
             detailUrl: "javascript: void(0);",
             description: ["复活赛门票"]
         },
-        specialSign: "/bower_components/SHARED-UI/assistFiles/onlineStore/img/hot.png"
-    };
-    var paymentDataStructure1 = {
-        title: "",
+        specialSign: cdnify("/bower_components/SHARED-UI/assistFiles/onlineStore/img/hot.png")
+    }, {
         offerId: "dcb33391-baad-4dd0-9c05-aa2e4f05c940",
-        price: "0.00",
-        details: [],
         forcusDetail: {
             detailUrl: "javascript: void(0);",
             description: ["复活赛门票", "１对５小组点评"]
         },
         specialSign: ""
-    };
-    var paymentDataStructure2 = {
-        title: "",
+    }, {
         offerId: "dcb33391-baad-4dd0-9c05-aa2e4f05c940",
-        price: "0.00",
-        details: [],
         forcusDetail: {
             detailUrl: "javascript: void(0);",
             description: ["复活赛门票", "１对１专家点评"]
         },
         specialSign: ""
-    };
-    var paymentDataStructure3 = {
-        title: "",
+    }, {
         offerId: "dcb33391-baad-4dd0-9c05-aa2e4f05c940",
-        price: "0.00",
-        details: [],
         forcusDetail: {
             detailUrl: "javascript: void(0);",
             description: ["复活赛门票", "１对１专家点评"]
         },
         specialSign: ""
-    };
-    var paymentDataStructure4 = {
-        title: "",
+    }, {
         offerId: "dcb33391-baad-4dd0-9c05-aa2e4f05c940",
-        price: "0.00",
-        details: [],
         forcusDetail: {
             detailUrl: "javascript: void(0);",
             description: ["复活赛门票", "１对１专家点评"]
         },
         specialSign: ""
-    };
-    var paymentList = [paymentDataStructure, paymentDataStructure1, paymentDataStructure2, paymentDataStructure3, paymentDataStructure4];
+    }];
 
     exports.onlineStoreParentCtrl = function ($scope, $state, service) {
         $scope.paymentList = paymentList;
@@ -70,13 +58,13 @@
                             o.title = data.name;
                             o.price = data.price;
                             o.details = [];
-                            
+
                             for (var i = 0; i < data.packages.length; i++) {
                                 for (var j = 0; j < data.packages[i].products.length; j++) {
                                     var p = data.packages[i].products[j];
                                     o.details.push({
                                         description: p.name,
-                                        icon: '/images/icons/product_type_' + p.productTypeId + '_red.png',
+                                        icon: cdnify('/images/icons/product_type_' + p.productTypeId + '_red.png'),
                                         mount: p.times
                                     });
                                 }
