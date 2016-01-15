603b22409c7f5cf4684d27381931190e5028825a
henry huang <happybridge14@163.com>
2016-01-15 18:05:25 +0800

    Update detail pattern

--- client/www/js/page/opportunity-detail/detail-favourite.js
+++ client/www/js/page/opportunity-detail/detail-favourite.js
@@ -1,13 +1,15 @@
 angular.module('opdModule').directive('bopdfavourite', function() {
-        return {
-            restrict: "E",
-            scope: {
-                src: '='
-            },
-            templateUrl: '/view-partial/opd/detail-favourite.html',
-            link: function($scope, element, attrs) {
-                $scope.data = {};
-                $scope.data.positions = [{
+    return {
+        restrict: "E",
+        scope: {
+            src: '='
+        },
+        templateUrl: '/view-partial/opd/detail-favourite.html',
+        link: function($scope, element, attrs) {
+            $scope.data = {};
+            $scope.data.positions = {
+                showPosition: true,
+                data: [{
                     matchLevel: "a",
                     progressRate: "50",
                     position: {
@@ -16,20 +18,35 @@ angular.module('opdModule').directive('bopdfavourite', function() {
                         salary: "1111",
                         certification: "c",
                     },
-                    issueTime: "2015",
-                    company: "ksjksdf"
-                },{
-                    matchLevel: "a",
-                    progressRate: "50",
-                    position: {
-                        name: "a",
-                        type: "b",
-                        salary: "1111",
-                        certification: "c",
-                    },
-                    issueTime: "2015",
-                    company: "ksjksdf"
-                }]
+                    issueTime: "2015-12-12",
+                    company: "ksjksdf",
+                    status: "finisheda",
+                    companyinfo: {
+                        logo: "",
+                        name: "",
+                        field: "",
+                        flag: ""
                     }
-                };  
-    });
+                },{
+                    matchLevel: "d",
+                    progressRate: "70",
+                    position: {
+                        name: "c",
+                        type: "d",
+                        salary: "111122",
+                        certification: "d",
+                    },
+                    status: "delivered",
+                    issueTime: "2015-12-20",
+                    company: "ksj ksdf",
+                    companyinfo: {
+                        logo: "",
+                        name: "",
+                        field: "",
+                        flag: ""
+                    }
+                }]
+            };
+        }
+    };  
+});
