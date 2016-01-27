fb486a79b27e92464d39d2aa691f7923c558ff8c
田杰 <jefftian@hcdlearning.com>
2016-01-27 19:34:41 +0800

    Clickable tags.

--- client/www/js/page/opportunity-detail/detail-training.js
+++ client/www/js/page/opportunity-detail/detail-training.js
@@ -13,6 +13,45 @@ angular.module('opdModule').directive('bopdtraining', function () {
             $('.ui.right.arrow.icon').on('click', function () {
                 $(this).closest('.ui.image').find('.shape').shape('flip right');
             });
+
+            $scope.settingData = {
+                sorting: 0,
+                filter: 0
+            };
+
+            $scope.settings = [{
+                name: 'sorting',
+                key: '排序方式：',
+                list: [{
+                    id: 0,
+                    text: '默认'
+                }, {
+                    id: 1,
+                    text: '最新'
+                }, {
+                    id: 2,
+                    text: '推荐星级'
+                }, {
+                    id: 3,
+                    text: '热门'
+                }]
+            }, {
+                name: 'filter',
+                key: '机构专长：',
+                list: [{
+                    id: 0,
+                    text: '不限'
+                }, {
+                    id: 1,
+                    text: '语言'
+                }, {
+                    id: 2,
+                    text: 'IT'
+                }, {
+                    id: 3,
+                    text: '留学'
+                }]
+            }];
         }
     };
 });
