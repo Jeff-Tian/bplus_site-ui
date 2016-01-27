eb86faadab626a82a9651c361f0fdd363bd1a1fc
kimi.zhang <kimi.zhang@ef.com>
2016-01-28 01:58:40 +0800

    opportunity-detail#job/corporation

---
+++ client/www/js/page/opportunity-detail/detail-job-corporation.js
@@ -0,0 +1,22 @@
+﻿angular
+    .module('opdModule')
+    .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('job/corporation', {
+            url: '/job/corporation/:corporationid',
+            templateUrl: 'job-corporation.html',
+            controller: 'detailJobCorporation'
+        });
+    }])
+    .directive('bopdjobcorporation', function () {
+        return {
+            templateUrl: '/view-partial/opd/detail-job-corporation.html',
+            link: function (scope, element, attrs) {
+                scope.$element = angular.element(element);
+            }
+        };
+    })
+    .controller('detailJobCorporation', ['$scope', function ($scope) {
+
+        $scope.chartPentagon = '4-2-3-2-2';
+
+    }]);
