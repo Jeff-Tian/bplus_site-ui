a84518cb4fcf80cc585498659a196768d0315884
henry huang <happybridge14@163.com>
2016-01-28 17:16:57 +0800

    Update slides

--- client/www/js/page/opportunity-detail/widget/traininglist/main.js
+++ client/www/js/page/opportunity-detail/widget/traininglist/main.js
@@ -2,25 +2,33 @@
     .module('opdModule')
     .directive('shapeFlip', function () {
         return {
-            link: function (scope, element, arrts) {
-                var $element = angular.element(element),
-                    $shape = $element.find('.shape'),
-                    $btnPreview = $element.find('.btn-preview'),
-                    isPreview = $btnPreview.find('i').hasClass('grey'),
-                    $btnNext = $element.find('.btn-next'),
-                    isNext = $btnNext.find('i').hasClass('grey');
-                if ($shape.length && $shape.shape) {
-                    $shape.shape();
-                    $btnPreview.on('click', function () {
-                        if (isPreview) {
-                            $shape.shape('flip left');
-                        }
-                    });
-                    $btnNext.on('click', function () {
-                        if (isNext) {
-                            $shape.shape('flip right');
-                        }
-                    });
+            link: {
+                post: function (scope, element, arrts) {
+                    var $element = angular.element(element),
+                        $shape = $element.find('.shape'),
+                        $btnPreview = $element.find('.btn-preview'),
+                        isPreview = $btnPreview.find('i').hasClass('grey'),
+                        $btnNext = $element.find('.btn-next'),
+                        isNext = $btnNext.find('i').hasClass('grey');
+                    var findShape = function() {
+                        return angular.element(element).find('.shape');
+                    }
+                    if ($shape.length && $shape.shape) {
+                        $shape.shape();
+                        $btnPreview.on('click', function () {
+                            var shape = findShape().shape();
+                            if (isPreview) {
+                                shape.shape('flip left');
+                            }
+                        });
+                        $btnNext.on('click', function () {
+                            var shape = findShape().shape();
+                            findShape().shape();
+                            if (isNext) {
+                                shape.shape('flip right');
+                            }
+                        });
+                    }
                 }
             }
         };
