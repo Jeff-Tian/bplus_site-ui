86fce379a4d1e8157ce7a4540567a945d503b223
henry huang <happybridge14@163.com>
2016-02-05 14:34:35 +0800

    Improve autocomplete

--- client/www/js/directives/autocomplete.js
+++ client/www/js/directives/autocomplete.js
@@ -1,22 +1,43 @@
 (function (exports) {
     exports.autoComplete = function ($http) {
+        var findScopeValue = function($scope, sourceStr) {
+            return sourceStr.split(".").reduce(function(target, key) {
+                if (target){
+                    return target[key];
+                } else {
+                    return null;
+                }
+            }, $scope)
+        }
         return {
-            link: function (scope, element, attrs, ngModel) {
+            link: function (scope, element, attrs) {
+                debugger;
                 var lng = angular.bplus.localeHelper.getLocale(window.location.pathname);
                 var url = attrs.datasourceBplusAutocomplete ? ('/mock/' + attrs.datasourceBplusAutocomplete.replace('.json', '.' + lng + '.json')) : undefined;
-
                 if (url) {
-                    $http.get(url).success(function (data) {
+                    var dataSource = findScopeValue(scope, url);
+                    if (dataSource) {
                         $(element).autocomplete({
-                            lookup: data,
+                            lookup: dataSource,
                             onSelect: function (suggestion) {
                                 var ngModelString = $(this).attr('ng-model');
-                                var a = ngModelString.split('.');
-
-                                scope[a[0]][a[1]] = suggestion.value;
+                                findScopeValue(scope, ngModelString) = suggestion.value;
                             }
                         });
-                    });
+                    } else {
+                        $http.get(url).success(function (data) {
+                            $(element).autocomplete({
+                                lookup: data,
+                                onSelect: function (suggestion) {
+                                    var ngModelString = $(this).attr('ng-model');
+                                    findScopeValue(scope, ngModelString) = suggestion.value;
+                                    // var a = ngModelString.split('.');
+
+                                    // scope[a[0]][a[1]] = suggestion.value;
+                                }
+                            });
+                        });
+                    }
                 }
             }
         };
