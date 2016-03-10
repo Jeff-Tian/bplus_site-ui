f4a256ccb4c39b03146b610672bdfcd25934e1c4
田杰 <jefftian@hcdlearning.com>
2016-03-10 15:40:46 +0800

    Add replace task to replace google fonts in client/dist/semantic css.

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,32 @@ gulp.task('hz', function (done) {
     runSequence('assemble', 'build', 'deploy-hz', done);
 });
 
-var buildOnlineStore = require('./node_modules/online-store/gulpfile.js');
+gulp.task('replace-bplus', function (done) {
+    var replace = require('gulp-replace');
+
+    return gulp.src(['client/dist/semantic/dist/semantic.min.css'])
+        .pipe(replace(/https:\/\/fonts\.googleapis\.com\/css/g, 'http://fonts.useso.com/css'))
+        .pipe(gulp.dest('client/dist/semantic/dist/'));
+});
+
+gulp.task('online-store-build', function (done) {
+    return require('./node_modules/online-store/gulpfile.js')(done);
+});
+
+gulp.task('run online-store-build', function (done) {
+    sh.exec('gulp online-store-build', done);
+});
+
+gulp.task('run replace-bplus', function (done) {
+    sh.exec('gulp replace-bplus', done);
+});
+
 gulp.task('release', function (done) {
-    buildOnlineStore(done);
+    // Here use sh.exec() to run 2 tasks to ensure
+    // the sequence not interrupted by the asynchronous events.
+    runSequence(
+        'run online-store-build',
+        'run replace-bplus',
+        done
+    );
 });
