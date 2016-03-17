42e9dbca4e91ecf67718720fbaf33ecb3dd55950
田杰 <jefftian@hcdlearning.com>
2016-03-17 19:19:34 +0800

    Integrate register and login.

--- routes/corp.js
+++ routes/corp.js
@@ -4,25 +4,42 @@ var path = require('path');
 var mixedViewEngine = require('./mixedViewEngine');
 var config = require('../config/index');
 var localeHelper = require('../locales/localeHelper.js');
+var membership = require('../serviceProxy/membership.js');
 
-function routerFactory(name, title) {
+function getRenderer(title, target) {
+    return function (req, res, next) {
+        res.locals.title = title + ' -- Bridge+';
+        res.locals.cdnify = function (url) {
+            if (url[0] === '/') {
+                url = url.substr(1);
+            }
+
+            return config.cdn.normal + url + '?' + config.cdn.version;
+        };
+        mixedViewEngine.render(res, 'corp/' + target + '.jade', 'layout.jade', res.locals);
+    };
+}
+
+function routerFactory(name, title, pipes) {
     var target = name;
 
     if (target === '') {
         target = 'index';
     }
 
-    corp.get(localeHelper.localePath('/' + name), function (req, res, next) {
-        var data = res.locals;
-        data.title = title + ' -- Bridge+';
-        mixedViewEngine.render(res, 'corp/' + target + '.jade', 'layout.jade', res.locals, data);
-    });
+    var args = [localeHelper.localePath('/' + name)];
+
+    if (pipes) {
+        args = args.concat(pipes);
+    }
+
+    args = args.concat([getRenderer(title, target)]);
+
+    corp.get.apply(corp, args);
 }
-//Index
+
 routerFactory("", '企业首页');
-//Regist
-routerFactory("register", '企业注册');
-//CV
+routerFactory("register", '企业注册', membership.ensureAuthenticated);
 routerFactory("cv", '简历管理');
 //Find
 routerFactory("find", '找人才');
@@ -34,5 +51,11 @@ mixedViewEngine
     .renderEJS(corp, '/reset-password')
     .renderEJS(corp, './set-password')
 ;
+
+corp.get(localeHelper.localePath('/sign-in'), getRenderer('企业登录 - Bridge+', 'index'));
+
+corp.get(localeHelper.localePath('/long'), function (req, res, next) {
+
+});
 
 module.exports = corp;
