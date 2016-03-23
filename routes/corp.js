76c8ca64255869ec0de4b0e9a3366f88b098d8d7
田杰 <jefftian@hcdlearning.com>
2016-03-23 18:33:13 +0800

    school and contact us for corp.

--- routes/corp.js
+++ routes/corp.js
@@ -50,6 +50,8 @@ routerFactory("find", '找人才');
 //Setting
 routerFactory("setting", '账户管理');
 routerFactory('about-us', '关于我们');
+routerFactory('school', '学校合作');
+routerFactory('contact-us', '联系我们');
 
 mixedViewEngine
     .renderEJS(corp, '/reset-password-by-email')
