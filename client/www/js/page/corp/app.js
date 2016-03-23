502390fd8e2cc8d77c108df92fe535ed4b62891e
田杰 <jefftian@hcdlearning.com>
2016-03-23 16:33:30 +0800

    Corp header.

--- client/www/js/page/corp/app.js
+++ client/www/js/page/corp/app.js
@@ -1,10 +1,41 @@
 angular.module('corpModule', ['bplusModule', 'widgetModule', 'bplusConfigModule', 'bridgeplus.corp'])
-    .run(['$rootScope', 'service', function ($rootScope, service) {
+    .value('corpModuleEvents', {
+        corpInfo: {
+            loaded: 'corpInfo:loaded'
+        }
+    })
+    .run(['$rootScope', 'service', 'DeviceHelper', 'corpModuleEvents', 'msgBus', function ($rootScope, service, DeviceHelper, corpModuleEvents, msgBus) {
         $rootScope.corpSignOut = function () {
             service.delete($rootScope.config.serviceUrls.corp.member.signOut)
                 .finally(function (res) {
                     window.location.href = '/';
                 });
         };
+
+        var corp_id = DeviceHelper.getCookie('corp_id');
+        if (corp_id) {
+            $rootScope.loadingData = false;
+            service.executePromiseAvoidDuplicate($rootScope, 'loadingData', function () {
+                return service.get($rootScope.config.serviceUrls.corp.member.basicInfo, {
+                    params: {
+                        company_id: corp_id
+                    }
+                });
+            }).then(function (result) {
+                $rootScope.corpBasicInfo = {
+                    companyName: result.name,
+                    city: result.location,
+                    license: {name: result.business_license_url},
+                    licenseInfo: result.business_license_url ? result.business_license_url : undefined,
+                    contact: result.contact,
+                    position: result.contact_position,
+                    email: result.contact_mail,
+                    mobile: result.contact_mobile,
+                    auditStatus: result.register_status
+                };
+
+                msgBus.emitMsg(corpModuleEvents.corpInfo.loaded, $rootScope.corpBasicInfo);
+            });
+        }
     }])
 ;
