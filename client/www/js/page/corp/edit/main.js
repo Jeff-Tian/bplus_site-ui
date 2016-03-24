cf26ea8f87ec78dc26945976733e9df05dc9875c
kimi.zhang <kimi.zhang@ef.com>
2016-03-25 02:33:59 +0800

    http://localhost:12001/edit

--- client/www/js/page/corp/edit/main.js
+++ client/www/js/page/corp/edit/main.js
@@ -6,19 +6,80 @@ angular
             //    '*': '='
             //},
             controller: ['$scope', function ($scope) {
-                $scope.openForm = function () {
-                    console.log('test');
+                $scope.showAccountInfo = true;
+                $scope.modalPassword = function () {
+                    $scope.$modalPassword && $scope.$modalPassword.modal('show');
+                };
+                $scope.modalTelephone = function () {
+                    $scope.$modalTelephone && $scope.$modalTelephone.modal('show');
                 };
             }],
             link: function (scope, element, attrs) {
-                //scope.$corpEdit = angular.element(element);
+                ;
             }
         };
     }])
-    .directive('corpEditTitle', ['$rootScope', function ($rootScope) {
+    .directive('formUsername', [function () {
         return {
             link: function (scope, element, attrs) {
-                console.log('test');
+                var $form = angular.element(element);
+                $form.form({
+                    on: 'blur',
+                    inline: true,
+                    fields: {
+                        username: {
+                            identifier: 'username',
+                            rules: [{
+                                type: 'empty',
+                                prompt: $form.find('[name=username]').attr('data-prompt')
+                            }]
+                        }
+                    }
+                }).on('submit', function () {
+                    if (!$form.hasClass('loading') && $form.form('is valid')) {
+                        $form.addClass('loading');
+                    }
+                    return false;
+                });
+            }
+        };
+    }])
+    .directive('modalPassword', [function () {
+        return {
+            link: function (scope, element, attrs) {
+                scope.$modalPassword = angular.element(element);
+            }
+        };
+    }])
+    .directive('modalTelephone', [function ($rootScope) {
+        return {
+            link: function (scope, element, attrs) {
+                scope.$modalTelephone = angular.element(element);
+            }
+        };
+    }])
+    .directive('formEmail', [function () {
+        return {
+            link: function (scope, element, attrs) {
+                var $form = angular.element(element);
+                $form.form({
+                    on: 'blur',
+                    inline: true,
+                    fields: {
+                        email: {
+                            identifier: 'email',
+                            rules: [{
+                                type: 'email',
+                                prompt: $form.find('[name=email]').attr('data-prompt')
+                            }]
+                        }
+                    }
+                }).on('submit', function () {
+                    if (!$form.hasClass('loading') && $form.form('is valid')) {
+                        $form.addClass('loading');
+                    }
+                    return false;
+                });
             }
         };
     }]);
