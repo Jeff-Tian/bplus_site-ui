2e8161dc88b452ff16e4621de508ff192ef79570
kimi.zhang <kimi.zhang@ef.com>
2016-03-27 02:23:01 +0800

    localhost:12001/edit

--- client/www/js/page/corp/edit/main.js
+++ client/www/js/page/corp/edit/main.js
@@ -1,25 +1,60 @@
 angular
     .module('corpModule')
-    .directive('corpEdit', ['$rootScope', function ($rootScope) {
+    .directive('corpEdit', [function () {
         return {
             //scope: {
             //    '*': '='
             //},
-            controller: ['$scope', function ($scope) {
+            controller: ['$scope', '$http', function ($scope, $http) {
+                $scope.completion = 0;
                 $scope.showAccountInfo = true;
+
                 $scope.modalPassword = function () {
                     $scope.$modalPassword && $scope.$modalPassword.modal('show');
                 };
                 $scope.modalTelephone = function () {
                     $scope.$modalTelephone && $scope.$modalTelephone.modal('show');
                 };
+
+                $http({
+                    url: '/js/page/corp/edit/data.json',
+                    method: 'GET'
+                }).then(function (response) {
+                    if (response && response.status == 200 && response.statusText && response.statusText.toString().toLowerCase() == 'ok' && response.data) {
+                        var data = response.data;
+                        if (data.completion) {
+                            $scope.completion = data.completion;
+                        }
+                        if (data.logo) {
+                            $scope.logo = data.logo;
+                        }
+                        if (data.username) {
+                            $scope.username = data.username;
+                        }
+                        if (data.telephone) {
+                            $scope.telephone = data.telephone.toString().replace(/^(\d{3})\d{6}(\d{2})$/, '$1' + '******' + '$2');
+                        }
+                        if (data.email) {
+                            $scope.email = data.email;
+                        }
+                        if (data.slogan) {
+                            $scope.slogan = data.slogan;
+                        }
+                        if (data.description) {
+                            $scope.description = data.description;
+                        }
+                        if (data.tags) {
+                            $scope.tags = data.tags;
+                        }
+                    }
+                });
             }],
             link: function (scope, element, attrs) {
                 ;
             }
         };
     }])
-    .directive('formUsername', [function () {
+    .directive('formUsername', ['$timeout', function ($timeout) {
         return {
             link: function (scope, element, attrs) {
                 var $form = angular.element(element);
@@ -38,6 +73,9 @@ angular
                 }).on('submit', function () {
                     if (!$form.hasClass('loading') && $form.form('is valid')) {
                         $form.addClass('loading');
+                        $timeout(function () {
+                            $form.removeClass('loading');
+                        }, 3000);
                     }
                     return false;
                 });
@@ -77,6 +115,45 @@ angular
                 }).on('submit', function () {
                     if (!$form.hasClass('loading') && $form.form('is valid')) {
                         $form.addClass('loading');
+                        $timeout(function () {
+                            $form.removeClass('loading');
+                        }, 3000);
+                    }
+                    return false;
+                });
+            }
+        };
+    }])
+    .directive('formBasicInfo', ['$timeout', function ($timeout) {
+        return {
+            link: function (scope, element, attrs) {
+                var $form = angular.element(element);
+                $form.on('submit', function () {
+                    if (!$form.hasClass('loading')) {
+                        $form.addClass('loading');
+                        var vals = $form.form('get values');
+                        console.log(vals);
+                        $timeout(function () {
+                            $form.removeClass('loading');
+                        }, 3000);
+                    }
+                    return false;
+                });
+            }
+        };
+    }])
+    .directive('formCompanyInfo', ['$timeout', function ($timeout) {
+        return {
+            link: function (scope, element, attrs) {
+                var $form = angular.element(element);
+                $form.on('submit', function () {
+                    if (!$form.hasClass('loading')) {
+                        $form.addClass('loading');
+                        var vals = $form.form('get values');
+                        console.log(vals);
+                        $timeout(function () {
+                            $form.removeClass('loading');
+                        }, 3000);
                     }
                     return false;
                 });
