53d5e32d88124ccb078b4e80a906730ed72d2278
田杰 <jefftian@hcdlearning.com>
2016-04-27 11:08:23 +0800

    Don't copy test to dist.

--- client/grunt/copy.js
+++ client/grunt/copy.js
@@ -2,7 +2,23 @@ module.exports = function () {
     return {
         main: {
             files: [
-                {expand: true, cwd: "<%= config.src %>", src: ['**'], dest: "<%= config.dist %>"},
+                {
+                    expand: true,
+                    cwd: "<%= config.src %>",
+                    src: [
+                        'api/**',
+                        'bower/**',
+                        'competion/**',
+                        'css/**',
+                        'icon/**',
+                        'img/**',
+                        'js/**',
+                        'mobile/**',
+                        'view-partial/**',
+                        '*'
+                    ],
+                    dest: "<%= config.dist %>"
+                },
                 {
                     expand: true,
                     cwd: "./locales/",
