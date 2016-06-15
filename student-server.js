682c0a1473376f341f247725845bbfb3027a90ad
田杰 <jefftian@hcdlearning.com>
2016-06-15 16:36:17 +0800

    Use title instead of description.

--- student-server.js
+++ student-server.js
@@ -181,8 +181,6 @@ require('./pre-check/education-background')(server);
 
 server.all('*', localeHelper.setLocale, localeHelper.setLocalVars);
 
-server.use('/', require('./serviceProxy/membership.js').setSignedInUser);
-
 function renderIndex(req, res, next) {
     renderOrRedirect(req, res, 'index');
 }
