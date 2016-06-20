09bea2fddfc3ba7d59d761c9d675c82559a5de0e
Robbin.Zhu <robbin@59124.com>
2016-06-20 16:18:30 +0800

    render portal page

--- routes/student-homepage.js
+++ routes/student-homepage.js
@@ -3,10 +3,11 @@ var fs = require('fs');
 var urlParser = require('url');
 var localeHelper = require('../locales/localeHelper.js');
 var supportedLocales = localeHelper.supportedLocales;
+var Portal = require('../client/www/bower/bridgeplus_ui');
 
 function renderIndex(req, res, next) {
     if (!mobileDetector.isRequestFromMobileOrPad(req)) {
-        return res.render(__dirname + '/../client/www/bower/bridgeplus_ui/public/index.html');
+        return Portal.render('zh', 'index', req, res, next);
     }
 
     tryRenderMobileTemplate('index', req, res);
@@ -41,13 +42,19 @@ function tryRenderMobileTemplate(template, req, res) {
     }
 }
 
-module.exports = function (server) {
+module.exports = function(server) {
     server.get('/', renderIndex);
-    server.get('/index', renderIndex);
-
-    supportedLocales.map(function (l) {
+    server.get('/index', function(req, res, next) {
+        res.redirect(301, '/');
+    });
+    server.get('/portal/:lang/:page', function(req, res, next) {
+        Portal.render(req.params.lang, req.params.page, req, res, next);
+    });
+    supportedLocales.map(function(l) {
         server.get('/' + l, renderIndex);
-        server.get('/' + l + '/index', renderIndex);
+        server.get('/' + l + '/index', function(req, res, next) {
+            res.redirect(301, '/');
+        });
     });
 
     server.get(localeHelper.localePath('/index-2015-2016', false), render20152016Index);
