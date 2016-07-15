c454bf89085a087457ac4aab12efa137b2133761
田杰 <jefftian@hcdlearning.com>
2016-07-15 14:42:27 +0800

    deposit.

--- config/config_prd.js
+++ config/config_prd.js
@@ -4,7 +4,7 @@ var config = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'config_prd.json
 
 if (process.env.DATACENTER) {
     config.captcha.host = process.env.DATACENTER + '-' + config.captcha.host;
-    config.payment.public.host = process.env.DATACENTER + '-' + config.payment.public.host;
+    config.payment.public.host = process.env.DATACENTER + 'bp-' + config.payment.public.host;
     config.service_upload = "//" + process.env.DATACENTER + '-' + config.upload.public.host + ":" + config.upload.public.port;
 }
 config.bplusServiceParams = require("./config_bplusService");
